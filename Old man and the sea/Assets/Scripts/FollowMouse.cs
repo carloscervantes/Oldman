c525b84246c7983162f7b430180caba225bb6b9d
Carlos Cervantes <cervantes.c@gmail.com>
2015-06-25 16:16:11 +0200

    Project added

---
+++ Old man and the sea/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour {
+
+	private Vector3 mouseposition;
+	public float moveSpeed = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(0)) {
+			mouseposition = Input.mousePosition;
+			mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+			transform.position = Vector2.Lerp(transform.position, mouseposition, moveSpeed);
+		}
+	
+	}
+}
