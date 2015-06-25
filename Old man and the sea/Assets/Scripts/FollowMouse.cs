f74ed8f03736458104b207d772586c683242700e
Carlos Cervantes <cervantes.c@gmail.com>
2015-06-25 16:54:28 +0200

    Scene Carlos creada (movimiento pez, camera follow).

--- Old man and the sea/Assets/Scripts/FollowMouse.cs
+++ Old man and the sea/Assets/Scripts/FollowMouse.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class FollowMouse : MonoBehaviour {
 
 	private Vector3 mouseposition;
-	public float moveSpeed = 0.01f;
+	public float moveSpeed = 0.04f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +14,14 @@ public class FollowMouse : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		mouseposition = Input.mousePosition;
+		mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
+		transform.position = Vector2.Lerp(transform.position, mouseposition, moveSpeed);
+
 		if (Input.GetMouseButton(0)) {
-			mouseposition = Input.mousePosition;
-			mouseposition = Camera.main.ScreenToWorldPoint(mouseposition);
-			transform.position = Vector2.Lerp(transform.position, mouseposition, moveSpeed);
+			moveSpeed = 0.1f;
 		}
+		else moveSpeed = 0.04f;
 	
 	}
 }
