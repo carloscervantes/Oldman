3ae29a01b7cc17732276d78459c11690029d81e3
Tanya Romero <tanya.romero@gmail.com>
2015-07-03 18:32:43 +0200

    Crecimiento del pez de nuevo.

---
+++ Old man and the sea/Assets/Assets_sceneTan/PlayerCollidersScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollidersScript : MonoBehaviour {
+
+	public GameObject MainGame;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		
+		if (coll.collider.gameObject.tag == "fish") {
+			//Debug.Log (coll.collider.gameObject.name);
+			Destroy (coll.collider.gameObject, 0);
+			MainGame.gameObject.GetComponent<MainScript3>().increment_fishes_consumed();
+		}
+		
+		
+	}
+
+
+}
