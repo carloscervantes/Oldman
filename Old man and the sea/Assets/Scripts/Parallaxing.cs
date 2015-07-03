6c7ae4a8230da8996726a7818777152f3a26b5cb
Carlos Cervantes <cervantes.c@gmail.com>
2015-07-03 16:50:42 +0200

    Background added.

--- Old man and the sea/Assets/Scripts/Parallaxing.cs
+++ Old man and the sea/Assets/Scripts/Parallaxing.cs
@@ -13,8 +13,8 @@ public class Parallaxing : MonoBehaviour {
 	//is called before Start(). Great for references.
 	void Awake () {
 		//set up the camera reference.
+		//cam = Camera.main.transform;
 		cam = Camera.main.transform;
-
 	}
 
 	// Use this for initialization
@@ -36,14 +36,14 @@ public class Parallaxing : MonoBehaviour {
 		for (int i = 0; i < BGelements.Length; i++) {
 			//Parallax is the opposite of the camera movement because the previous frame multiplied by scale.
 			float parallaxx = (previousCamPos.x -cam.position.x) * Parallax[i];
-			float parallaxy = (previousCamPos.y -cam.position.y) * Parallax[i];
+			//float parallaxy = (previousCamPos.y -cam.position.y) * Parallax[i];
 
 			// set a target  x position which is the current position plus  the parallax 
 			float backgroundTargetPosX = BGelements[i].position.x + parallaxx;
-			float backgroundTargetPosY = BGelements[i].position.y + parallaxy;
+			//float backgroundTargetPosY = BGelements[i].position.y + parallaxy;
 
 			//Create  a target position which is the backgrounds current position with its target x position
-			Vector3 backgroundTargetPos =  new Vector3 (backgroundTargetPosX, backgroundTargetPosY, BGelements[i].position.z);
+			Vector3 backgroundTargetPos =  new Vector3 (backgroundTargetPosX, BGelements[i].position.y, BGelements[i].position.z);
 
 			//Lerp between current position and the target position
 			BGelements[i].position = Vector3.Lerp (BGelements[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
